9ac1fa5a9f59612455fac12ade0eed2d5365c8d6
tommy-leap <tomohira07@icloud.com>
2019-12-04 14:28:42 +0900

    修正しました

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,13 @@
 $(function(){ 
      function buildHTML(message){
-      if ( message.image ) {
+
+      if ( message.image ){
+        var message_image =  `<img src=${message.image} >`
+      }else{
+        var message_image = ""
+      }
+
+ 
         var html =
          `<div class="message" data-message-id=${message.id}>
             <div class="upper-message">
@@ -16,29 +23,12 @@ $(function(){
                 ${message.content}
               </p>
             </div>
-            <img src=${message.image} >
+            ${message_image}
           </div>`
         return html;
-      } else {
-        var html =
-         `<div class="message" data-message-id=${message.id}>
-            <div class="upper-message">
-              <div class="upper-message__user-name">
-                ${message.user_name}
-              </div>
-              <div class="upper-message__date">
-                ${message.date}
-              </div>
-            </div>
-            <div class="lower-message">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
-            </div>
-          </div>`
-        return html;
-      };
     }
+      
+    
 $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
